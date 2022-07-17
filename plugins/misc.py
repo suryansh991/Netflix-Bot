3d8a354fcf7bdecb6a869b4c3464f2c6aa27492b
suryansh991 <70124389+suryansh991@users.noreply.github.com>
2022-07-17 11:42:43 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -65,9 +65,7 @@ async def song(bot, update):
 async def about(bot, update):
     await update.reply_photo(
             photo="https://te.legra.ph/file/dcaebfc72b12e13fb91a4.jpg",
-            caption='✯ Mʏ Nᴀᴍᴇ : BᴀᴛMᴀɴ
-✯ Cʀᴇᴀᴛᴏʀ : Sᴜʀʏᴀɴsᕼ
-✯Mᴀɪɴ Gʀᴏᴜᴘ [Aᴜᴛᴏ Rᴇǫᴜᴇsᴛ Mᴏᴠɪᴇs]⚡️'
+            caption='✯ Mʏ Nᴀᴍᴇ : BᴀᴛMᴀɴ✯ Cʀᴇᴀᴛᴏʀ : Sᴜʀʏᴀɴsᕼ ✯Mᴀɪɴ Gʀᴏᴜᴘ [Aᴜᴛᴏ Rᴇǫᴜᴇsᴛ Mᴏᴠɪᴇs]⚡️'
     )
 
 @Client.on_message(filters.command('source'))
